20b1bc38a36d4845889f442d612e039fc3e5d5b9
co0ki3 <ehrrhgus0324@gmail.com>
2021-07-02 09:58:14 +0900

    WebDriver Link Modify

--- selenium_tutorial1.py
+++ selenium_tutorial1.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
 
-dv = webdriver.Chrome(r'C:\Users\co0ki3\Desktop\Python\Types\Selenium\chromedriver_win32\chromedriver.exe')
+dv = webdriver.Chrome(r'C:\Users\co0ki3\Desktop\Python\Selenium\chromedriver_win32\chromedriver.exe')
 dv.get("https://www.google.com")
 
 input_box = dv.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
